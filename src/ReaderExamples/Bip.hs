059d7837c0f4fc3399bbc46122c437f4340e51e3
Francososa <franky.sosa@gmail.com>
2020-07-09 23:16:39 -0600

    Simple monad ladder

---
+++ src/ReaderExamples/Bip.hs
@@ -0,0 +1,21 @@
+module ReaderExamples.Bip where
+
+import Control.Applicative
+
+boop = (*2)
+doop = (+10)
+
+bip :: Integer -> Integer
+bip = boop . doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
+
+boopDoop :: Integer -> Integer
+boopDoop = do
+    a <- boop
+    b <- doop
+    return (a + b)

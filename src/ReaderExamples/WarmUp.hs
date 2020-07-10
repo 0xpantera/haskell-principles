609b384b13b6321900f0888f28e78bf32a4174cc
Francososa <franky.sosa@gmail.com>
2020-07-09 23:17:37 -0600

    Warm up exercise

---
+++ src/ReaderExamples/WarmUp.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE InstanceSigs #-}
+module ReaderExamples.WarmUp where
+
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap cap rev
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> id <*> composed
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = do
+    og <- id
+    res <- fmapped
+    return (og, res)
+
+tupled'' :: [Char] -> ([Char], [Char])
+tupled'' = 
+    id >>= 
+        \og -> fmapped >>= 
+            \res -> return (og, res)
+
+
+newtype Reader r a =
+    Reader { runReader :: r -> a }
+
+ask :: Reader a a
+ask = Reader id
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader $ f
+
+instance Functor (Reader r) where
+    fmap :: (a -> b) -> Reader r a -> Reader r b
+    fmap f (Reader ra) = Reader $ f . ra
+
+instance Applicative (Reader r) where
+    pure :: a -> Reader r a
+    pure x = Reader $ const x
+
+    (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+    (Reader rab) <*> (Reader ra) = Reader $ \x -> rab x (ra x)

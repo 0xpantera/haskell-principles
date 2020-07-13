bc51d8739dd69700dac770e418c324f39e4bbe81
Francososa <franky.sosa@gmail.com>
2020-07-13 11:11:04 -0600

    Use difference list for fizzbuzz

--- src/StateExamples/FizzBuzz.hs
+++ src/StateExamples/FizzBuzz.hs
@@ -2,6 +2,8 @@ module StateExamples.FizzBuzz where
 
 import Control.Monad
 import Control.Monad.Trans.State
+import qualified Data.DList as DL
+
 
 fizzBuzz :: Integer -> String
 fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
@@ -9,17 +11,16 @@ fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
            | n `mod` 3 == 0 = "Fizz"
            | otherwise = show n
 
-fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList :: [Integer] -> DL.DList String
 fizzbuzzList list =
-    execState (mapM_ addResult list) []
+    execState (mapM_ addResult list) DL.empty
 
-addResult :: Integer -> State [String] ()
+addResult :: Integer -> State (DL.DList String) ()
 addResult n = do
     xs <- get
     let result = fizzBuzz n
-    put (result : xs)
+    put (DL.snoc xs result)
 
 main :: IO ()
 main =
-    mapM_ putStrLn $
-        reverse $ fizzbuzzList [1..100]
+    mapM_ putStrLn $ fizzbuzzList [1..100]

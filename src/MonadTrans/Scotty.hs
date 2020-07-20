6f9a091824c8a525b4b9bddb486b52260aa3cb46
Francososa <franky.sosa@gmail.com>
2020-07-19 22:35:25 -0600

    Handroll lifts for scotty ActionT lift

--- src/MonadTrans/Scotty.hs
+++ src/MonadTrans/Scotty.hs
@@ -3,14 +3,28 @@
 module MonadTrans.Scotty where
 
 import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT(..))
+import Control.Monad
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Lazy hiding (get)
 import Data.Monoid (mconcat)
+
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT m = ReaderT (const m)
 
 main :: IO ()
 main = scotty 3000 $ do
     get "/:word" $ do
         beam <- param "word"
-        lift (putStrLn "hello")
+        (ActionT
+         . (ExceptT . liftM Right)
+         . (\m -> ReaderT $ const m)
+         . (\m -> StateT $ \s -> do
+                    a <- m
+                    return (a, s))) 
+            (putStrLn "hello")
 
         html $
             mconcat ["<h1>Scotty, ",
